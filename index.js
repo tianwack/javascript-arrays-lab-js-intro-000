99d3e6ac06f1e78c215032249758ec6c115b1e0a
Christian Wacker <christianwwacker@gmail.com>
2020-01-10 04:23:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function prependKitten(name){
   return [name, ...kittens]
 }
 
-function removeLastKitten(name){
-  return name.slice(0, kittens.length - 1)
+function removeLastKitten(){
+    return kittens.slice(0,kittens.length-1)
 }
 
 function removeFirstKitten(name){
